6757d9a5d84abef02658703ed28722ed0a6aeb50
Prajit Viswanadha <83452847+V-prajit@users.noreply.github.com>
2024-07-05 21:58:34 +0530

    Added DB tags and auto migrations

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -3,10 +3,7 @@ from django.db import models
 
 class User(AbstractUser):
     profile_picture = models.ImageField(upload_to='profile_pics/', null = True, blank = True)
-    USER_TYPES = (
-        ('regular', 'Regular User'),
-        ('admin', 'Admin'),
-    )
+
 
     def __str__(self):
         return f"{self.username} {self.email}"
--- backend/storage/models.py
+++ backend/storage/models.py
@@ -1,16 +1,21 @@
 from django.db import models
 from django.conf import settings
 
-# Create your models here.
+class Tag(models.Model):
+    tag = models.CharField(max_length= 100, unique=True)
+
+    def __str__(self):
+        return self.tag
+
 class Transaction(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     details = models.CharField(max_length = 6)
-    posting_date = models.DateField()
+    posting_date = models.DateField(db_index=True)
     description = models.TextField()
     amount = models.DecimalField(max_digits = 15, decimal_places=2)
     transaction_type = models.CharField(max_length=100)
     balance = models.DecimalField(max_digits = 15, decimal_places=2)
     check_or_slip = models.CharField(max_length=100, blank=True, null=True)
-
+    tags = models.ManyToManyField(Tag, blank=True)
     class Meta:
         unique_together = ('details', 'posting_date', 'description', 'amount', 'transaction_type', 'balance', 'check_or_slip')
