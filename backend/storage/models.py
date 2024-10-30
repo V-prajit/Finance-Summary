fe7a0b7ba7172401d0fb860a03d85eb74aa482b4
Prajit Viswanadha <prajit.viswanadha.06@gmail.com>
2024-10-30 09:40:19 -0500

    setting up docker

--- backend/storage/models.py
+++ backend/storage/models.py
@@ -7,7 +7,7 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 class Tag(models.Model):
-    tag = models.CharField(max_length=100, unique=True)
+    tag = models.CharField(max_length=100, unique=True, null=True, blank=True)
 
     def __str__(self):
         return self.tag
